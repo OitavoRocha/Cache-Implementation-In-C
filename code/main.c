d389384c90838fb430e2607029dcc87e36360607
Otávio Rocha <otaviosrocha30@gmail.com>
2023-04-17 18:54:25 -0300

    Update main.c

--- code/main.c
+++ code/main.c
@@ -1,14 +1,15 @@
 /*
         ERROS ENCONTRADOS:
-    nao esta abrindo o arquivo na função mapeada diretamente
+    os resultados de todos os mapeamentos estao incorretos
+    o problema muito provavelmente está relacionado ao index
 
 */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
 #include <stdint.h>
+#include <time.h>
 
 // declaration of global variables for offset, tag and index bits
 int nBitsOffset;
@@ -23,9 +24,10 @@ typedef struct _cache * Cache;
 
 void readCommandLine( int * nSets, int * bSize, int * assoc, char * subst, int * flagOut, char * arquivoEntrada, int argc, char const ** argv ); // function to read from the command line
 uint32_t reverseAddress( int address );
-void fullyAssociative( int nSets, int bSize, int assoc, char * subst, char * arquivoEntrada, int * accesses, int * compulsory, int * capacity );
+int randomSwap( int assoc );
+void fullyAssociative( int nSets, int bSize, int assoc, char * subst, char * arquivoEntrada, int * accesses, int * hit, int * misses, int * compulsory, int * capacity );
 void directMapped( int nSets, int bSize, int assoc, char * arquivoEntrada, int * accesses, int * hit, int * misses, int * compulsory, int * conflict );
-void setAssociative( int nSets, int bSize, int assoc, char * subst, char * arquivoEntrada, int * accesses, int * compulsory, int * capacity, int * conflict );
+void setAssociative( int nSets, int bSize, int assoc, char * subst, char * arquivoEntrada, int * accesses, int * hit, int * misses, int * compulsory, int * capacity, int * conflict );
 
 int main( int argc, char const ** argv ) {
     int nSets = 256, bSize = 4, assoc = 1, flagOut = 1;
@@ -35,9 +37,6 @@ int main( int argc, char const ** argv ) {
 
     //readCommandLine( &nSets, &bSize, &assoc, subst, &flagOut, arquivoEntrada, argc, argv );
 
-    uint32_t n = reverseAddress( 0x12345678 );
-    printf("0x%08x\n", n);
-
     // calculates the numbers of bits for each one
     nBitsOffset = ( int ) log2( bSize );
     nBitsIndex = ( int ) log2( nSets );
@@ -45,14 +44,13 @@ int main( int argc, char const ** argv ) {
 
 
     // one function for each mapping
-    if ( nSets == 1 && assoc != 1 )
-        return 0;
-        //fullyAssociative( nSets, bSize, assoc, subst, arquivoEntrada, &accesses, &compulsory, &capacity );
-    else if ( assoc == 1 )
+    if ( nSets == 1 && assoc != 1 ) {
+        fullyAssociative( nSets, bSize, assoc, subst, arquivoEntrada, &accesses, &hit, &misses, &compulsory, &capacity );
+    } else if ( assoc == 1 ) {
         directMapped( nSets, bSize, assoc, arquivoEntrada, &accesses, &hit, &misses, &compulsory, &conflict );
-    else
-        return 0;
-        //setAssociative( nSets, bSize, assoc, subst, arquivoEntrada, &accesses, &compulsory, &capacity, &conflict );
+    } else {
+        setAssociative( nSets, bSize, assoc, subst, arquivoEntrada, &accesses, &hit, &misses, &compulsory, &capacity, &conflict );
+    }
 
 
     hitRate = ((float)hit/(float)accesses);
@@ -74,7 +72,7 @@ int main( int argc, char const ** argv ) {
         return 0;
     }
 
-    printf("\n%d, %.2f, %.2f, %.2f, %.2f, %.2f\n", accesses, hitRate, missRate, compRate, capRate, confRate );
+    printf("\n%d, %.4f, %.4f, %.2f, %.2f, %.2f\n", accesses, hitRate, missRate, compRate, capRate, confRate );
 
     return 0;
 }
@@ -116,41 +114,50 @@ uint32_t reverseAddress( int address ) {
     uint32_t mask = 0xff000000;
     uint32_t reversed = 0;
 
-    reversed |= ((address<<(6*4))&mask);
-    reversed |= ((address<<(2*4))&(mask>>(2*4)));
-    reversed |= ((address>>(2*4))&(mask>>(4*4)));
-    reversed |= ((address>>(6*4))&(mask>>(6*4)));
+    reversed |= ((address<<(24))&mask);
+    reversed |= ((address<<(8))&(mask>>(2*4)));
+    reversed |= ((address>>(8))&(mask>>(4*4)));
+    reversed |= ((address>>(24))&(mask>>(6*4)));
 
     return reversed;
 }
-    
 
-void directMapped( int nSets, int bSize, int assoc, char * arquivoEntrada, int * accesses, int * hit, int * misses, int * compulsory, int * conflict ) {
-    Cache c1 = (Cache)malloc( sizeof( struct _cache ) * ( nSets * assoc ) );
+int randomSwap( int assoc ) {
+    srand(time(NULL));
+
+    return rand() % assoc;
+} 
+
+void directMapped( int nSets, int bSize, int assoc, char * arquivoEntrada, int * accesses, int * hit, int * misses, int * compulsory, int * conflict ) 
+{
+    Cache c1 = (Cache)malloc( sizeof( struct _cache ) * (nSets) );
     FILE *p;
     uint32_t end;
     uint32_t read; // variable to read from file
     uint32_t tag;
     uint32_t index;
 
-    for ( int i = 0 ; i < nSets * assoc ; i++ ) 
+    for ( int i = 0 ; i < nSets ; i++ ) 
          c1[i].val = 0;
          
 
-    p = fopen( "bin_100.txt" , "rb" );
+    p = fopen( "bin_100.txt" , "r" );
     if ( !p ) {
         perror("Impossivel abrir o arquivo: ");
         exit(1);
     }
 
+    int n;
 
     printf("reading file...\n");
     while( fscanf(p, "%008x", &read) == 1 ) {
         end = reverseAddress( read );
         tag = end >> (nBitsOffset + nBitsIndex);
-        index = ( end >> (nBitsOffset) ) & ( (int)pow(2, nBitsIndex) - 1 );
+        index = ( end >> (nBitsOffset) );
+        n = (int) pow(2, nBitsIndex) - 1 ;
+        index &= n;
 
-        printf("%d %d %d\n", nBitsOffset, nBitsIndex, nBitsTag);
+        //printf("%d %d %d\n", nBitsOffset, nBitsIndex, nBitsTag);
 
         (*accesses)++;
 
@@ -171,4 +178,128 @@ void directMapped( int nSets, int bSize, int assoc, char * arquivoEntrada, int *
 
     fclose( p );
     free( c1 ); // liberates allocated memory
+}
+
+void setAssociative( int nSets, int bSize, int assoc, char * subst, char * arquivoEntrada, int * accesses, int * hit, int * misses, int * compulsory, int * capacity, int * conflict )
+{
+    FILE *p;
+    uint32_t end;
+    uint32_t read; // variable to read from file
+    uint32_t tag;
+    uint32_t index;
+    Cache * c1 = (Cache *)malloc( sizeof( Cache ) * nSets );
+    int hitFlag;
+    int isFull;
+
+    for ( int i = 0 ; i < nSets ; i++ )
+        c1[i] = (Cache)malloc( sizeof( struct _cache ) * assoc );
+    
+    for ( int i = 0 ; i < nSets ; i++ )
+        for ( int j = 0 ; j < assoc ; j++ )
+            c1[i][j].val = 0;
+
+
+    p = fopen( "bin_10000.txt" , "r" );
+    if ( !p ) {
+        perror("Impossivel abrir o arquivo: ");
+        exit(1);
+    }
+
+    printf("reading file...\n");
+    while( fscanf(p, "%008x", &read) == 1 ) {
+        end = reverseAddress( read );
+        tag = end >> (nBitsOffset + nBitsIndex);
+        index = ( end >> (nBitsOffset) ) & ( (int)pow(2, nBitsIndex) - 1 );
+        hitFlag = 0;
+
+        (*accesses)++;
+
+        for( int i = 0 ; i < assoc ; i++ ) {
+            if ( c1[index][i].val != 1 ) {
+                (*misses)++;
+                (*compulsory)++;
+                c1[index][i].val = 1;
+                c1[index][i].tag = tag;
+                i = assoc; // to end loop
+                hitFlag++;
+            } else if ( c1[index][i].tag == tag ) {
+                (*hit)++;
+                i = assoc; // to end loop
+                hitFlag++;
+            }
+            if( i == assoc - 1 ) {
+                isFull = 1; // if loop goes through the entire matrix
+            }
+        }
+
+        if ( hitFlag == 0 ) {
+            (*misses)++;
+            if ( isFull ) 
+                (*capacity)++;
+            else
+                (*conflict)++;
+            c1[index][randomSwap( assoc )].tag = tag;
+        }
+    }
+}
+
+void fullyAssociative( int nSets, int bSize, int assoc, char * subst, char * arquivoEntrada, int * accesses, int * hit, int * misses, int * compulsory, int * capacity ){
+    Cache c1 = (Cache)malloc( sizeof( struct _cache ) * (assoc) );
+    FILE *p;
+    uint32_t end;
+    uint32_t read; // variable to read from file
+    uint32_t tag;
+    int isFull = 0;
+    int isIn;
+
+    for ( int i = 0 ; i < assoc ; i++ ) 
+         c1[i].val = 0;
+         
+
+    p = fopen( "vortex.in.sem.persons.txt" , "r" );
+    if ( !p ) {
+        perror("Impossivel abrir o arquivo: ");
+        exit(1);
+    }
+
+
+    printf("reading file...\n");
+    while( fscanf(p, "%008x", &read) == 1 ) {
+        end = reverseAddress( read );
+        tag = end >> (nBitsOffset + nBitsIndex);
+        //index = ( end >> (nBitsOffset) ) & ( (int)pow(2, nBitsIndex) - 1 );
+        isIn = 0;
+
+        (*accesses)++;
+
+        if ( isFull == 0 ) {
+            for ( int i = 0 ; i < assoc ; i++ ) {
+                if ( c1[i].val == 0 ) {
+                    (*compulsory)++;
+                    (*misses)++;
+                    c1[i].val = 1;
+                    c1[i].tag = tag;
+                    i = assoc; // to end loop
+                } else if ( c1[i].tag == tag ) {
+                    (*hit)++;
+                    i = assoc; // to end loop
+                }
+                if ( i == assoc-1 )
+                    isFull = 1;
+            }
+        } else {
+            for ( int i = 0 ; i < assoc ; i++ ) {
+                if ( c1[i].tag == tag ) {
+                    (*hit)++;
+                    isIn++;
+                    i = assoc;
+                }
+            }
+            if ( isIn == 0 ) {
+                (*capacity)++;  // in fully associative mapping there are only capacity misses
+                (*misses)++;
+                c1[randomSwap(assoc)].tag = tag;
+            }
+        }
+    }
 }
